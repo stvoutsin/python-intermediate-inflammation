38aed8487c90829827a9ca8fdd8febc1d1e935b9
stvoutsin <steliosvoutsinas@yahoo.com>
2021-12-07 11:06:47 +0000

    Added more testing for the patient_normalise class

--- tests/test_models.py
+++ tests/test_models.py
@@ -36,3 +36,26 @@ def test_daily_min(test, expected):
     """Test min function works for array of zeroes and positive integers."""
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+@pytest.mark.parametrize(
+    "test, expected, raises",
+    [
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+    ])
+def test_patient_normalise(test, expected, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if raises:
+        with pytest.raises(raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
